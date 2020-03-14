8f8ac77e4d32a3a66c468cffc5649b376e7878bc
AYB-cmd <aeljelladi@gmail.com>
2020-03-14 23:36:18 +0100

    workshop-3

--- workshop-3/script/script.js
+++ workshop-3/script/script.js
@@ -2,15 +2,17 @@
 
 function convert() {
  var x = "";
- var y = 120;
+ var y = prompt("Enter a number: ");
   
   while ( y > 0) {
       x +=y % 2; 
       y = Math.floor(y / 2);
-    
+     
   };
+  
   x = x.split("").reverse();
   return x
 };
+
 
 console.log(convert());
